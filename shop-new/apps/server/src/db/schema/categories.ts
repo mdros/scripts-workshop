0e8ed0ef03cd696acc25da181a94e0db1517e4c5
Michał Droś <michal.dros01@gmail.com>
2024-05-05 14:38:45 +0200

    chore: remove old files

--- shop-new/apps/server/src/db/schema/categories.ts
+++
@@ -1,9 +0,0 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const categories = sqliteTable("categories", {
-	id: integer("id").primaryKey({ autoIncrement: true }),
-	name: text("name").unique().notNull(),
-});
-
-export type SelectCategory = typeof categories.$inferSelect;
-export type InsertCategory = typeof categories.$inferInsert;
